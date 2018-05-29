fd893890c74da3379a2ac8dd181c80b05b2b5896
Sajjad Haider <sani723@hotmail.com>
2018-05-29 07:45:31 +0400

    Connect to mLab
    Online mongoose db, connect and open the connection

--- server/index.js
+++ server/index.js
@@ -4,6 +4,12 @@ const resolvers = require("./resolvers/index");
 const mongoose = require("mongoose");
 
 
+mongoose.connect('mongodb://sajjad:test123@ds237620.mlab.com:37620/gql-ninja');
+mongoose.connection.once("open", () => {
+  console.log("Connected to database");
+});
+
+
   // In the most basic sense, the ApolloServer can be started
   // by passing type definitions (typeDefs) and the resolvers
   // responsible for fetching the data for those types.
