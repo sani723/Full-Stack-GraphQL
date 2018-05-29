83181c4256a43aad8bfdd5c7932b948fdc137b67
Sajjad Haider <sani723@hotmail.com>
2018-05-29 19:44:59 +0400

    Add Apollo client
    Install packages required for Apollo client, setup new Apollo client and wrap the root component with ApolloProvider

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
+
+// Apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="main">
-
-      </div>
+      <ApolloProvider client={client}>
+        <div className="main">
+          <h1>Latest Tweets</h1>
+        </div>
+      </ApolloProvider>
     );
   }
 }
